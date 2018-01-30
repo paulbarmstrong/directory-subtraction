d45a03e7556a39aed7d02c9a180155a7d25ae755
paulbarmstrong <paulbarmstrong@users.noreply.github.com>
2018-01-29 19:15:13 -0500

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# directory-subtraction
+# Directory Subtraction Script
+
+## Details
+
+* Author: Paul Armstrong
+* Date: March 2017 (but updated and uploaded more recently)
+* Progress: 100%
+
+## Description
+
+This little script will "subtract" one directory from the other. What I mean by this is any files found in both directories will be deleted from the target directory. Folders found in both directories will only be deleted if there were no other common files in that folder.
+
+## Usage
+
+The script prompts the user for a directory to "blacklist" and a directory to remove blacklisted files from. Blacklisted files also found in the second directory will be marked for deletion. A warning prompt will display the full paths of the first ten on the deletion list and provide the total number of files to be deleted. If the user confirms, the files will be deleted.
+
+Even though I personally use this script and have no problems, any script which involves deleting directories has the potential for disaster. Because of this, I have decided not to include an executable of the script.
+
+There are two main reasons why the script *SHOULD* be safe:
+* The script can only mark for deletion files common to both directories (And they can't both be the same directory)
+* The script gives a warning including the full path of the first ten files to be deleted, and gives the total number of files to be deleted
+
+Feel free to take a look at the code inside the *source_code* folder and compile it at your own risk.
+
+## Why
+
+The reason why I made this script as a way to undo a manual installation of files at a later date. This script allows me to "subtract" an installation quickly, no matter how many files the installation includes.
+
+Another reason why I wanted to make this script was to personally investigate the use of recursion for manipulating file systems.
---
+++ source_code/DirectorySubtraction.java
@@ -0,0 +1,130 @@
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+
+/**
+Author: Paul Armstrong
+Original Date: March 2017
+Description:
+	This script will scan the contents of a first directory to produce a
+	black-list. That black-list will be used to remove corresponding
+	files from a second directory. When this happens, the script is
+	"subtracting" the first directory from the second directory.
+	
+	The reason why I made this script as a way to undo a manual
+	installation of files at a later date. This script allows me to
+	"subtract" an installation quickly, no matter how many files or
+	sub-directories the installation includes.
+
+
+**/
+
+public class DirectorySubtraction
+{
+	public static void main(String[] args)
+	{
+		// Define unknown files empty ahead of time to establish scope and allow a .exists() check
+		File referenceFolder =	new File("");
+		File targetFolder =		new File("");
+		
+		// Prompt for the folder containing the blacklisted file system
+		JOptionPane.showMessageDialog(null,"Select the folder containing files to be blacklisted:");
+		
+		// Use a file chooser to get folder location input
+		JFileChooser fc = new JFileChooser();
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		if (fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+			referenceFolder = fc.getSelectedFile();
+		
+		
+		// Prompt for the folder to remove blacklisted files from
+		JOptionPane.showMessageDialog(null,"Select the folder to remove blacklisted files from:");
+		
+		// Use a file chooser to get folder location input
+		fc = new JFileChooser();
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		if (fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+			targetFolder = fc.getSelectedFile();
+		
+		if (referenceFolder.exists() && targetFolder.exists() && !(referenceFolder.equals(targetFolder)))
+		{
+			// Do a post order traversal of the file system (so folders are deleted last)
+			ArrayList<File> referenceFiles = new ArrayList<File>();
+			recursiveFileTraversal(referenceFiles, referenceFolder);
+			
+			// If the corresponding file in the targetFolder exists, mark for deletion
+			ArrayList<File> deleteList = new ArrayList<File>();
+			String referencePath = fixFileString(referenceFolder.getAbsolutePath());
+			String targetPath = fixFileString(targetFolder.getAbsolutePath());
+			System.out.println(referencePath);
+			for (File file : referenceFiles)
+			{
+				File coFile = new File(fixFileString(file.getAbsolutePath())
+												.replaceAll(referencePath, targetPath));
+				if (coFile.exists())
+				{
+					deleteList.add(coFile);
+				}
+			}
+			
+			// Construct a warning based on the contents of deleteList
+			String delFileWarning = 
+					"Type 'yes' to delete the following "+deleteList.size()+" files:\n";
+			int i=0;
+			while (i<deleteList.size() && i<10)
+			{
+				delFileWarning += deleteList.get(i).toString()+"\n";
+				i++;
+			}
+			if (i == 10)
+			{
+				delFileWarning += "and "+(deleteList.size()-i)+" more.";
+			} else if (i == 0) {
+				delFileWarning += "None";
+			}
+			String input = JOptionPane.showInputDialog(delFileWarning+"\n");
+			
+			// If yes, start deleting files
+			if (input.equals("yes"))
+			{
+				for (File file : deleteList)
+				{
+					// File class shouldn't delete a folder that isn't empty, but just in case
+					if (!file.isDirectory() || !(file.listFiles().length > 0))
+					{
+						file.delete();
+					}
+				}
+				JOptionPane.showMessageDialog(null,"Files have been removed.");
+			} else {
+				JOptionPane.showMessageDialog(null,"Deletion Canceled");
+			}
+			
+		} else {
+			JOptionPane.showMessageDialog(null, "Invalid Folders. Operation Aborted.");
+		}
+	}
+	
+	private static void recursiveFileTraversal(ArrayList<File> referenceFiles, File folder)
+	{
+		// Recursively traverse and record all files within referenceFolder
+		File[] listOfFiles = folder.listFiles();
+		for (File childFile : listOfFiles)
+		{
+			if (childFile.isDirectory())
+			{
+				recursiveFileTraversal(referenceFiles,childFile);
+			}
+			referenceFiles.add(childFile);
+		}
+	}
+	
+	public static String fixFileString(String str)
+	{
+		// To make java regex happy
+		return str.replace("\\", "/");
+	}
+}
